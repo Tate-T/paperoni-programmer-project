b2a0de79240eab4666cbd004d28fa3f83ab55df9
Артем <141495242+Artem-Dykyi@users.noreply.github.com>
2025-01-02 12:07:04 +0200

    max-num-function

---
+++ src/our-js/max-number.js
@@ -0,0 +1,27 @@
+const inputs = document.querySelectorAll(".max-box-item");
+const maxText = document.querySelector(".max-text")
+
+const maxList = [];
+console.log(maxList);
+
+inputs.forEach(input => {
+    input.addEventListener("change", (event) => {
+        const num = event.target.value;
+        maxList.push(num);
+        // event.target.value = ""; 
+
+        let maxNumber = maxList[0];
+        console.log(maxList)
+        
+        for (let i = 0; i < maxList.length; i++) {
+            if (maxList[i] > maxNumber) {
+                maxNumber = maxList[i]; 
+            }
+        }
+
+        maxText.textContent = `Найбільше число, яке ви ввели  ${maxNumber}`;
+    });
+});
+
+
+
