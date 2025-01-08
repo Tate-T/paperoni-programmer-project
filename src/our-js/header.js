5e15f4e086e388539e16e11a509c337a96ec1117
Артем <141495242+Artem-Dykyi@users.noreply.github.com>
2025-01-08 15:28:06 +0200

    some change

--- src/our-js/header.js
+++ src/our-js/header.js
@@ -1,17 +1,25 @@
 const dropdownBtn = document.querySelector(".header-dropdown-btn");
 const dropdownList = document.querySelector(".header-dropdown-content");
 const cirkle = document.querySelector(".header-cirkle")
+const img = document.querySelector(".header-cirkle-img")
 const body = document.querySelector("body")
+
 
 dropdownBtn.addEventListener("click", () => {
     dropdownList.classList.toggle("show");
 });
 
 cirkle.addEventListener('click', event => {
-    event.target.classList.toggle('time-calculator-cirkle-right');
-    if (event.target.classList.contains('time-calculator-cirkle-right')) {
+    cirkle.classList.toggle('header-cirkle-right');
+
+    if (cirkle.classList.contains('header-cirkle-right')) {
       body.style.backgroundColor = "red"
+      img.src = "../img/moon.svg"
+      cirkle.style.backgroundColor ="black"
     } else {
+      img
        body.style.backgroundColor = "blue"
+      img.src = "../img/sun.svg"
+      cirkle.style.backgroundColor ="white"
     }
   });
