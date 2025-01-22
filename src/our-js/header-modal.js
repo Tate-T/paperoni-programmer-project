ad2297000456d19adc119f9f1b8e212414e5e9f5
Pizzaman333 <andrijmalchevv369@gmail.com>
2025-01-22 21:34:54 +0200

    fix modal

--- src/our-js/header-modal.js
+++ src/our-js/header-modal.js
@@ -11,32 +11,31 @@ const modalSub = document.querySelector(".header-modal-sub")
 const btnClose = document.querySelector(".header-modal-btn-close")
 const btnSubClose = document.querySelector(".header-modal-sub-btn-close")
 
-// btnSave.addEventListener("click", () => {
-//     if (input.value.length >= 3) {
-//         headerHello.textContent = `Вітаємо ${input.value}!`;
-//         input.style.border = "1px solid green";
-//       } else {
-//         headerHello.textContent = `Вітаємо ${input.value} !`
-//         input.style.border = "1px solid red";
-//       }
-    
-// })
-
 btnSave.addEventListener("click", () => {
     if (input.value.length >= 3) {
-      headerHello.textContent = `Вітаємо ${input.value}!`;
-    } else {
-      alert("Введіть щонайменше 3 символи!");
-    }
-  });
+        headerHello.textContent = `Вітаємо ${input.value}!`;
+        input.style.border = "1px solid green";
+      } else {
+        headerHello.textContent = `Вітаємо, User!`
+        input.style.border = "1px solid red";
+      }
+})
 
-  input.addEventListener("input", () => {
-    btnSave.disabled = input.value.length < 3;
-  });
+// btnSave.addEventListener("click", () => {
+//     if (input.value.length >= 3) {
+//       headerHello.textContent = `Вітаємо ${input.value}!`;
+//     } else {
+//       alert("Введіть щонайменше 3 символи!");
+//     }
+//   });
+
+//   input.addEventListener("input", () => {
+//     btnSave.disabled = input.value.length < 3;
+//   });
   
-  btnSave.addEventListener("click", () => {
-    headerHello.textContent = `Вітаємо ${input.value}!`;
-  });
+//   btnSave.addEventListener("click", () => {
+//     headerHello.textContent = `Вітаємо ${input.value}!`;
+//   });
   
 
 
--- src/our-js/header.js
+++ src/our-js/header.js
@@ -1,124 +1,118 @@
-const dropdownBtn = document.querySelector(".header-dropdown-btn");
-const dropdownList = document.querySelector(".header-dropdown-content");
-const cirkle = document.querySelector(".header-cirkle")
-const elipse = document.querySelector(".header-ellipse")
+const dropdownBtn = document.querySelector('.header-dropdown-btn');
+const dropdownList = document.querySelector('.header-dropdown-content');
+const cirkle = document.querySelector('.header-cirkle');
+const elipse = document.querySelector('.header-ellipse');
 
-const cirkleCalcul = document.querySelector(".time-calculator-cirkle")
-const elipseCalcul = document.querySelector(".time-calculator-ellipse")
-const img = document.querySelector(".header-cirkle-img")
-const body = document.querySelector("body")
+const cirkleCalcul = document.querySelector('.time-calculator-cirkle');
+const elipseCalcul = document.querySelector('.time-calculator-ellipse');
+const img = document.querySelector('.header-cirkle-img');
+const body = document.querySelector('body');
 
+const allSection = document.querySelectorAll('section');
+const allText = document.querySelectorAll('p');
+const allHeader = document.querySelector('header');
+const allFoot = document.querySelector('footer');
+const allLink = document.querySelector('li');
 
-const allSection = document.querySelectorAll("section")
-const allText = document.querySelectorAll("p")
-const allHeader = document.querySelector("header")
-const allFoot = document.querySelector("footer")
-const allLink = document.querySelector("li")
+const allTitleH1 = document.querySelectorAll('h1');
+console.log(allTitleH1);
+const allTitleH2 = document.querySelectorAll('h2');
+console.log(allTitleH2);
+const allTitleH3 = document.querySelectorAll('h3');
 
-const allTitleH1 = document.querySelectorAll("h1")
-console.log(allTitleH1)
-const allTitleH2 = document.querySelectorAll("h2")
-console.log(allTitleH2)
-const allTitleH3 = document.querySelectorAll("h3")
-
-
-dropdownBtn.addEventListener("click", () => {
-    dropdownList.classList.toggle("show");
+dropdownBtn.addEventListener('click', () => {
+  dropdownList.classList.toggle('show');
 });
 
 cirkle.addEventListener('click', event => {
-    cirkle.classList.toggle('header-cirkle-right');
+  cirkle.classList.toggle('header-cirkle-right');
 
-    if (cirkle.classList.contains('header-cirkle-right')) {
-      // body.style.backgroundColor = "red"
-      img.src = "../img/moon.svg"
-      cirkle.style.backgroundColor ="black"
-      elipse.style.backgroundColor = "#FFF"
+  if (cirkle.classList.contains('header-cirkle-right')) {
+    body.style.backgroundColor = '#8a8a8a';
+    img.src = '../img/moon.svg';
+    cirkle.style.backgroundColor = 'black';
+    elipse.style.backgroundColor = '#FFF';
 
-      elipseCalcul.style.backgroundColor = "#FFF"
+    elipseCalcul.style.backgroundColor = '#FFF';
 
-      allText.forEach(text => {
-        text.classList.toggle("all-text-white")
-        text.classList.toggle("all-text-black")
-      })
+    allText.forEach(text => {
+      text.classList.toggle('all-text-white');
+      text.classList.toggle('all-text-black');
+    });
 
+    allTitleH1.forEach(titleH1 => {
+      titleH1.classList.toggle('all-title-white');
+      titleH1.classList.toggle('all-title-black');
+    });
 
-      allTitleH1.forEach(titleH1 => {
-        titleH1.classList.toggle("all-title-white")
-        titleH1.classList.toggle("all-title-black")
-      })
+    allTitleH2.forEach(titleH2 => {
+      titleH2.classList.toggle('all-title-white');
+      titleH2.classList.toggle('all-title-black');
+    });
 
-      allTitleH2.forEach(titleH2 => {
-        titleH2.classList.toggle("all-title-white")
-        titleH2.classList.toggle("all-title-black")
-      })
+    allTitleH3.forEach(titleH3 => {
+      titleH3.classList.toggle('all-title-white');
+      titleH3.classList.toggle('all-title-black');
+    });
 
+    allSection.forEach(section => {
+      section.classList.toggle('all-section-black');
+      section.classList.toggle('all-section-white');
+    });
 
-      allTitleH3.forEach(titleH3 => {
-        titleH3.classList.toggle("all-title-white")
-        titleH3.classList.toggle("all-title-black")
-      })
+    allHeader.classList.toggle('all-section-black');
+    allFoot.classList.toggle('all-section-black');
 
-      allSection.forEach(section => {
-        section.classList.toggle("all-section-black")
-        section.classList.toggle("all-section-white")
-      })
+    cirkleCalcul.classList.toggle('.all-circle-bg-white');
+    cirkleCalcul.classList.toggle('.all-circle-bg-black');
 
-      allHeader.classList.toggle("all-section-black")
-      allFoot.classList.toggle("all-section-black")
+    allLink.forEach(link => {
+      link.classList.toggle('all-title-white');
+      link.classList.toggle('all-title-black');
+    });
+  } else {
+    img;
+     body.style.backgroundColor = "white"
+    img.src = '../img/sun.svg';
+    cirkle.style.backgroundColor = 'white';
+    elipse.style.backgroundColor = '#7a7a7a';
 
-      cirkleCalcul.classList.toggle(".all-circle-bg-white")
-      cirkleCalcul.classList.toggle(".all-circle-bg-black")
+    elipseCalcul.style.backgroundColor = '#7a7a7a';
 
-      allLink.forEach(link => {
-        link.classList.toggle("all-title-white")
-        link.classList.toggle("all-title-black")
-      })
-      
-    } else {
-      img
-      //  body.style.backgroundColor = "blue"
-      img.src = "../img/sun.svg"
-      cirkle.style.backgroundColor ="white"
-      elipse.style.backgroundColor = "#7a7a7a"
+    allText.forEach(text => {
+      text.classList.toggle('all-text-white');
+      text.classList.toggle('all-text-black');
+    });
 
-      elipseCalcul.style.backgroundColor = "#7a7a7a"
+    allTitleH1.forEach(titleH1 => {
+      titleH1.classList.toggle('all-title-white');
+      titleH1.classList.toggle('all-title-black');
+    });
 
+    allTitleH2.forEach(titleH2 => {
+      titleH2.classList.toggle('all-title-white');
+      titleH2.classList.toggle('all-title-black');
+    });
 
-      allText.forEach(text => {
-        text.classList.toggle("all-text-white")
-        text.classList.toggle("all-text-black")
-      })
+    allTitleH3.forEach(titleH3 => {
+      titleH3.classList.toggle('all-title-white');
+      titleH3.classList.toggle('all-title-black');
+    });
 
-      allTitleH1.forEach(titleH1 => {
-        titleH1.classList.toggle("all-title-white")
-        titleH1.classList.toggle("all-title-black")
-      })
+    allSection.forEach(section => {
+      section.classList.toggle('all-section-black');
+      section.classList.toggle('all-section-white');
+    });
 
-      allTitleH2.forEach(titleH2 => {
-        titleH2.classList.toggle("all-title-white")
-        titleH2.classList.toggle("all-title-black")
-      })
+    allHeader.classList.toggle('all-section-black');
+    allFoot.classList.toggle('all-section-black');
 
-      allTitleH3.forEach(titleH3 => {
-        titleH3.classList.toggle("all-title-white")
-        titleH3.classList.toggle("all-title-black")
-      })
+    cirkleCalcul.classList.toggle('.all-circle-bg-white');
+    cirkleCalcul.classList.toggle('.all-circle-bg-black');
 
-      allSection.forEach(section => {
-        section.classList.toggle("all-section-black")
-        section.classList.toggle("all-section-white")
-      })
-
-      allHeader.classList.toggle("all-section-black")
-      allFoot.classList.toggle("all-section-black")
-
-      cirkleCalcul.classList.toggle(".all-circle-bg-white")
-      cirkleCalcul.classList.toggle(".all-circle-bg-black")
-
-      allLink.forEach(link => {
-        link.classList.toggle("all-link-white")
-        link.classList.toggle("all-link-black")
-      })
-    }
-  });
+    allLink.forEach(link => {
+      link.classList.toggle('all-link-white');
+      link.classList.toggle('all-link-black');
+    });
+  }
+});
