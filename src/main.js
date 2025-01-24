13641ddd0f95be0f0960ef37e232567ab62d3468
Pizzaman333 <andrijmalchevv369@gmail.com>
2025-01-24 18:02:13 +0200

    google dino fix

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
-import "./our-js/common.js"
-import "./our-js/header-modal.js"
-import "./our-js/header.js";
-import "./our-js/born-year.js";
-import "./our-js/guss-number.js";
-import "./our-js/rock-paper-scissors.js";
-import "./our-js/calculator.js";
-import "./our-js/time-calculator.js";
+// import "./our-js/common.js"
+// import "./our-js/header-modal.js"
+// import "./our-js/header.js";
+// import "./our-js/born-year.js";
+// import "./our-js/guss-number.js";
+// import "./our-js/rock-paper-scissors.js";
+// import "./our-js/calculator.js";
+// import "./our-js/time-calculator.js";
 import "./our-js/dinosaur-google.js"; 
-import "./our-js/football.js";
-import "./our-js/max-number.js";
-import "./our-js/team.js";
-import "./our-js/scientists.js";
-import "./our-js/space.js";
-import "./our-js/footer.js";
+// import "./our-js/football.js";
+// import "./our-js/max-number.js";
+// import "./our-js/team.js";
+// import "./our-js/scientists.js";
+// import "./our-js/space.js";
+// import "./our-js/footer.js";
