a3e199826c7594a4a0cc9e97bcba6a87f3e16a35
Артем <141495242+Artem-Dykyi@users.noreply.github.com>
2025-01-25 11:57:49 +0200

    some-change

--- src/our-js/max-number.js
+++ src/our-js/max-number.js
@@ -1,19 +1,31 @@
-const inputs = document.querySelectorAll(".max-number-box-item");
-const maxText = document.querySelector(".max-number-text")
+// const inputs = document.querySelectorAll(".max-number-box-item");
+// const maxText = document.querySelector(".max-number-text")
 
-const maxList = [];
+// const maxList = [];
+
+// inputs.forEach(input => {
+//     input.addEventListener("input", (event) => {
+//         const num = Number(event.target.value);
+//         maxList.push(num);
+        
+//         const maxNumber = Math.max(...maxList);
+
+//         maxText.textContent = `Найбільше число, яке ви ввели  ${maxNumber}`;
+//         // event.target.value = ""; 
+//     });
+// });
+
+
+
+const inputs = document.querySelectorAll(".max-number-box-item");
+const maxText = document.querySelector(".max-number-text");
 
 inputs.forEach(input => {
-    input.addEventListener("input", (event) => {
-        const num = Number(event.target.value);
-        maxList.push(num);
-        
-        const maxNumber = Math.max(...maxList);
+    input.addEventListener("input", () => {
+        const values = Array.from(inputs).map(item => Number(item.value) || 0);
 
-        maxText.textContent = `Найбільше число, яке ви ввели  ${maxNumber}`;
-        // event.target.value = ""; 
+        const maxNumber = Math.max(...values);
+
+        maxText.textContent = `Найбільше число, яке ви ввели: ${maxNumber}`;
     });
 });
-
-
-
